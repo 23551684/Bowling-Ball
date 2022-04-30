55ef386cfd030ae6c1515350a5215517a00ae3b4
erenkaratas77 <erenkaratas@stu.khas.edu.tr>
2022-05-01 01:51:10 +0300

    01.49
    en sonki değişiklik hürmüze gelince küçülme, listeden bulup parent null yapılacak

--- Bowling/Assets/Scripts/ball.cs
+++ Bowling/Assets/Scripts/ball.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ball : MonoBehaviour
 {
     public float speed;
     public GameObject prefRagdoll;
-
+    public LevelManager levelManager;
+    public ParticleSystem blood;
+         
     Vector3 mousePosition=Vector3.zero;
-
+    public GameObject[] humans;
     float smooth = 5.0f;
     float rotx;
     float rotz;
+    float backForce;
 
     void Start()
     {
@@ -21,7 +25,34 @@ public class ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        controlBall();
+
+        if (transform.localScale.x < 0.3)
+        {
+
+            StartCoroutine(dead());
+        }
+
+        int t = 0;
+        for(int i=0; i < transform.childCount; i++)
+        {
+            if(transform.GetChild(i).gameObject.tag=="prefRagdoll")
+            {
+                
+                humans[t] = transform.GetChild(i).gameObject;
+                t++;
+            }
+        }
         
+    }
+
+     public void spwanRagdoll()
+    {
+        var newChar = Instantiate(prefRagdoll, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        newChar.transform.parent = gameObject.transform;
+    }
+    void controlBall()
+    {
         float horizontal = 0;
 
 
@@ -34,40 +65,65 @@ public class ball : MonoBehaviour
             horizontal = (Input.mousePosition.x - mousePosition.x) / Screen.width * 1.5f;
             mousePosition = Input.mousePosition;
         }
-        
 
 
-        transform.position += new Vector3(0, 0, 1) * Time.deltaTime * speed  + new Vector3(1, 0, 0) * horizontal * 4;
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -3f, 3f), transform.position.y, transform.position.z);
+
+        transform.position += new Vector3(0, 0, 1) * Time.deltaTime * -speed * backForce + new Vector3(1, 0, 0) * horizontal * -2f;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -1.2f, 1.2f), transform.position.y, transform.position.z);
+        backForce = Mathf.Clamp(backForce + Time.deltaTime * 3.25f, -1, 1);
 
         if (horizontal > 0)
         {
-            rotz += -2;
-        }
-        else if(horizontal<0)
-        {
             rotz += 2;
+        }
+        else if (horizontal < 0)
+        {
+            rotz += -2;
         }
         else
         {
             rotz = 0;
         }
-        rotx +=2;
+        rotx -= 2;
         Quaternion target = Quaternion.Euler(rotx, 0, rotz);
         transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
     }
-
-     public void spwanRagdoll()
+    
+    private void OnTriggerEnter(Collider other)
     {
-        var newChar = Instantiate(prefRagdoll, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-        newChar.transform.parent = gameObject.transform;
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "collectableHuman")
+        if (other.gameObject.tag == "collectableHuman")
         {
-            Destroy(collision.gameObject);
+            Destroy(other.gameObject);
             spwanRagdoll();
         }
+        if (other.gameObject.tag == "makeItBigger")
+        {
+            transform.DOMoveY(transform.position.y + 0.1f, 1);
+            transform.DOScale(transform.localScale.x + 0.2f, 1);
+            Destroy(other.gameObject);
+            spwanRagdoll();
+        }
+        if (other.gameObject.tag == "obstacle")
+        {
+            print(humans.Length);
+            humans[humans.Length-1].transform.parent = null;
+            backForce = -3;
+            transform.DOMoveY(transform.position.y - 0.025f, 1);
+            transform.DOScale(transform.localScale.x - 0.05f, 1);
+
+        }
+        if (other.gameObject.tag == "flame")
+        {
+            transform.DOMoveY(transform.position.y - 0.025f, 1);
+            transform.DOScale(transform.localScale.x - 0.005f, 1);
+        }
+    }
+
+    IEnumerator dead()
+    {
+        levelManager.fail();
+        blood.Play();
+        yield return new WaitForSeconds(.4f);
+        Destroy(transform.gameObject);
     }
 }
