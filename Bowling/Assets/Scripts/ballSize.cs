c7b25b24fbb19701897d56c1c8100c724a608cb0
Zuhal13 <zuhalcantali@outlook.com.tr>
2022-05-01 12:58:03 +0300

    labut-ball changes
    1

---
+++ Bowling/Assets/Scripts/ballSize.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ballSize : MonoBehaviour
+{
+    public GameObject Ball;
+    float ball_Size = 0.2f;
+    float personValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "person")
+        {
+            personValue = other.gameObject.GetComponent<personValue>().operationValue;
+            ball_Size += personValue;
+            Ball.transform.DOScale((ball_Size), 1);
+        }
+    }
+}
---
+++ Bowling/Assets/Scripts/takedownLabuts.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class takedownLabuts : MonoBehaviour
+{
+    private int labutCount;
+    private float movementSpeed = 5f;
+    public Rigidbody rb;
+    private bool isKinematic;
+    private bool detectCollisions;
+    float ballSize = 0.2f;
+    float personValue;
+
+    public Animator canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //update the position
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+
+        //output to log the position change
+        Debug.Log(transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (ballSize < 10 && labutCount < ballSize)
+        {
+            EnableRigidBody();
+        }
+
+        if (ballSize > 10 && labutCount < ballSize)
+        {
+            EnableRigidBody();
+        }
+
+        if (labutCount > ballSize)
+        {
+            canvas.SetTrigger("lose");
+        }
+    }
+
+    void EnableRigidBody()
+    {
+        rb.isKinematic = true;
+        rb.detectCollisions = true;
+    }
+    void DisableRigidBody()
+    {
+        rb.isKinematic = false;
+        rb.detectCollisions = true;
+    }
+}
